1914ea2d9a83bf8c80e45fc9542e5d398cc5ce20
nanaki13 <jonathan.bonnet@laposte.net>
2021-04-30 23:49:28 +0200

    core: todo (remove cyclic dep)

--- src/main/scala/bon/jo/html/HtmlEventDef.scala
+++ src/main/scala/bon/jo/html/HtmlEventDef.scala
@@ -2,13 +2,16 @@ package bon.jo.html
 
 import org.scalajs.dom.raw.{Event, FocusEvent, HTMLElement, KeyboardEvent, MouseEvent}
 
+import scala.scalajs.js
+
 
 object HtmlEventDef {
 
   trait CaseEvent[A <: Event] {
-    def apply(html: HTMLElement, a: A => Unit): Unit = {
-      html.addEventListener[A](toString, a)
-
+    def apply(html: HTMLElement, a: A => Unit): js.Function1[A , _] = {
+      val f :  js.Function1[A, _] = a
+      html.addEventListener(toString, f)
+       f
     }
   }
 
@@ -25,10 +28,11 @@ object HtmlEventDef {
 trait HtmlEventDef {
   val html: HTMLElement
 
-  def $blur: (FocusEvent => Unit) => Unit = HtmlEventDef.blur(html, _)
-  def $click: (MouseEvent => Unit) => Unit = HtmlEventDef.click(html, _)
-  def $keyup: (KeyboardEvent => Unit) => Unit = HtmlEventDef.keyup(html, _)
-  def $change: (Event => Unit) => Unit = HtmlEventDef.change(html, _)
+  def $blur: (FocusEvent => Unit) =>  js.Function1[FocusEvent, _] = HtmlEventDef.blur(html, _)
+  def $click: (MouseEvent => Unit) => js.Function1[MouseEvent, _] = HtmlEventDef.click(html, _)
+
+  def $keyup: (KeyboardEvent => Unit) => js.Function1[KeyboardEvent, _]  = HtmlEventDef.keyup(html, _)
+  def $change: (Event => Unit) =>  js.Function1[Event, _] = HtmlEventDef.change(html, _)
   def $Action(doThis: => Unit): Unit = {
     $keyup{e => if(e.keyCode == 13) doThis}
   }
